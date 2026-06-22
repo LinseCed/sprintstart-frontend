2c4374683a9cbafefc41ff0f8128c81ce43961b4
Daniel <danius.maier@gmail.com>
2026-06-22 18:32:02 +0200

    adapted login page design

---
+++ src/keycloak-theme/login/styleLevelCustomization.tsx
@@ -0,0 +1,26 @@
+/**
+ * This file has been claimed for ownership from @keycloakify/login-ui version 250004.7.2.
+ * To relinquish ownership and restore this file to its original content, run the following command:
+ *
+ * $ npx keycloakify own --path "login/styleLevelCustomization.tsx" --revert
+ */
+
+import type { ReactNode } from "react";
+import type { ClassKey } from "@keycloakify/login-ui/useKcClsx";
+import "./login.css";
+import "../../styles/index.css"
+
+type Classes = { [key in ClassKey]?: string };
+
+type StyleLevelCustomization = {
+  doUseDefaultCss: boolean;
+  classes?: Classes;
+  loadCustomStylesheet?: () => void;
+  Provider?: (props: { children: ReactNode }) => ReactNode;
+};
+
+export function useStyleLevelCustomization(): StyleLevelCustomization {
+  return {
+    doUseDefaultCss: true,
+  };
+}
