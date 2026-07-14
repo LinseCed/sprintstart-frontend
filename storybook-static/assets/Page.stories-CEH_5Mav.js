1249219365a11f63b10bdec41a3e3d68387c9089
Daniil Perkin <danetschka4pertschik@gmail.com>
2026-07-14 14:06:42 +0200

    chore: remove storybook-static from git tracking and ignore it

--- storybook-static/assets/Page.stories-CEH_5Mav.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./KcPageStory-YOO-k3qL.js";var r,i,a,o,s;e((()=>{t(),{KcPageStory:r}=n({pageId:`frontchannel-logout.ftl`}),i={title:`login/frontchannel-logout.ftl`,component:r},a={},o={args:{kcContext:{logout:{clients:[]}}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    kcContext: {
-      logout: {
-        clients: []
-      }
-    }
-  }
-}`,...o.parameters?.docs?.source}}},s=[`Default`,`WithoutRedirectUrl`]}))();export{a as Default,o as WithoutRedirectUrl,s as __namedExportsOrder,i as default};
